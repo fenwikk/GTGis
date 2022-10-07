0b0d082e83e77215addaf00678fe37b081225733
Filip Mårtensson <fenwikk@protonmail.com>
2022-10-07 21:49:37 +0200

    Made Error spit out to msg

--- Error.h
+++ Error.h
@@ -2,13 +2,20 @@
 
 #include <exception>
 #include <string>
+#include <Windows.h>
 
 class Error : public std::exception {
 private:
     std::string message;
 
 public:
-    Error(std::string msg) : message(msg) {}
+    Error(std::string msg) : message(msg) {
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+        SetConsoleTextAttribute(hConsole, 12);
+        std::cerr << msg << "\n";
+        SetConsoleTextAttribute(hConsole, 7);
+    }
     std::string what() {
         return message;
     }
